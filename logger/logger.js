46c8796362b6921203366ce869f2f2bd607f5266
Tidjini <tidjini.messaoudi@gmail.com>
2023-02-01 12:01:41 +0100

    create logger module

--- logger/logger.js
+++ logger/logger.js
@@ -1,12 +1,33 @@
 const fs = require("fs");
 
+const RESULTS = {
+  INTERCEPTION: "INTERCEPTION",
+  FOREWORDING: "FOREWORDING",
+  GET_RESULTS: "GET RESULTS",
+  SEND_RESULTS: "SEND RESULTS",
+};
+
+class Log {
+  date = Date.now();
+  content = "";
+  constructor(username = "Unknowen", context = "GLOBAL", fun = "Unknowen") {
+    this.content = `${date} | ${username} | ${context}  | ${fun}  | `;
+    this.buildResult = this.buildResult.bind(this);
+  }
+
+  buildResult(result) {
+    this.content += `${RESULTS.INTERCEPTION} SUCCESS\n`;
+    return this.content;
+  }
+}
+
 //date and time auto-generated
 //user is givin or undefined, who call the service (ip address, mac)
 //context (procom-livriason)
 //function (avoir le total de tel produit (br) de 31/01/2020 a 31/01/2031)
 //results (fonction traitement correct ou pas) interception .. foreword .. get result .. send result success | failed ..
 
-const content = `01/02/2023 : 11:42 |   Tidjini |   PROCOM-LIVRAISON    |   Avoir total de BR de 31/01/2020 à 31/01/2031    |   SEND RESULTS SUCCESS\n`;
+const content = Log().buildResult();
 
 const EXCEPTION_PREFIX = "Exception, due to ";
 

6f0efb3b483c189709c1b9ca596ee3a2d41c9cec
Tidjini <tidjini.messaoudi@gmail.com>
2023-02-01 16:18:06 +0100

    Roadmap

--- apps/procom/index.js
+++ apps/procom/index.js
@@ -30,6 +30,22 @@ class Procom {
       result: `DISCONNECTED SUCCESS`,
     });
   }
+
+  static onReceiveRequest() {
+    //when recieving request from client
+  }
+
+  static onForewording() {
+    //on forwording
+  }
+
+  static onReceiveResponse() {
+    //on Receiving Response From Local service
+  }
+
+  static onSendResponse() {
+    //on Send Response To Remote client
+  }
 }
 
 module.exports = {

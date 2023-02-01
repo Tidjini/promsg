06a9ee9cf8c781f679235e81d89d482a29455760
Tidjini <tidjini.messaoudi@gmail.com>
2023-02-01 15:56:05 +0100

    set nodemon

--- apps/home/index.js
+++ apps/home/index.js
@@ -1,9 +1,24 @@
+const { Logger } = require("../../logger");
+
 function index(request, response) {
   response.sendFile(__dirname + "/index.html", (err) => {
     if (err) {
-      console.log("EXception handling", err);
       response.send(err.toString());
+      Logger.log({
+        username: "THIS",
+        context: "[HOME]",
+        verb: "INDEX",
+        result: `PAGE SENDED FAILD\n${err}\n`,
+      });
+      return;
     }
+
+    Logger.log({
+      username: "THIS",
+      context: "[HOME]",
+      verb: "INDEX",
+      result: "PAGE SENDED SUCCESS",
+    });
   });
 }
 

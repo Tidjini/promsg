dd62ccf7499d9b22d8fc7e8d9b4358e3f3e34fc1
Tidjini <tidjini.messaoudi@gmail.com>
2023-02-02 13:53:20 +0100

    check without the global scope in index.html

--- test/test_connection.js
+++ test/test_connection.js
@@ -2,11 +2,8 @@ const { io } = require("socket.io-client");
 
 const socket = io("ws://localhost:7777/", {
   reconnectionDelayMax: 10000,
-  //   auth: {
-  //     token: "123",
-  //     username: "tidjini",
-  //   },
-  //   query: {
-  //     "my-key": "my-value",
-  //   },
+  auth: {
+    token: "123",
+    username: "tidjini",
+  },
 });

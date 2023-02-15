6b68b54ffa8ac0313be2859b24bf7edf853a4674
Tidjini <tidjini.messaoudi@gmail.com>
2023-02-15 16:42:10 +0100

    set gateway service

---
+++ starter.bat
@@ -0,0 +1,6 @@
+c:\
+call pm2 kill
+cd c:\Projects\node\promsg
+call pm2 start gateway.js
+cd C:\Users\tidjini.messaoudi\source\repos\ConsoleApp1\ConsoleApp1\bin\x86\Release\net6.0
+call ConsoleApp1.exe
